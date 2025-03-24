978d9f451c3632e862cee8a1bae73798b1d2917c
jagallob <jagallob@eafit.edu.co>
2025-03-24 17:58:15 -0500

    Resueltos conflictos en UserController

--- ExtraHours.API/Model/ChangePasswordRequest.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ExtraHours.API.Model
-{
-    public class ChangePasswordRequest
-    {
-        public required string CurrentPassword { get; set; }
-        public required string NewPassword { get; set; }
-    }
-}
