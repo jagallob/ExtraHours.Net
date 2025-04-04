3263d9072c12b3001b8f92c6489019811f6e20ac
jagallob <jagallob@eafit.edu.co>
2025-04-03 19:51:17 -0500

    Agregar archivos de test y actualizar algunos estilos

--- client/src/components/ExtraHoursMenu.jsx
+++ client/src/components/ExtraHoursMenu.jsx
@@ -1,5 +1,7 @@
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../utils/useAuth";
+import ChangePasswordModal from "../components/ChangePasswordModal/ChangePasswordModal";
 import "./ExtraHoursMenu.scss";
 import Agregar from "../assets/images/Agregar.png";
 import Configuracion from "../assets/images/Configuracion.png";
@@ -8,7 +10,19 @@ import Informes from "../assets/images/Informes.png";
 
 const ExtraHoursMenu = () => {
   const navigate = useNavigate();
-  const { auth } = useAuth();
+  const { auth, logout } = useAuth();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = () => setIsModalOpen(true);
+  const closeModal = () => setIsModalOpen(false);
+
+  const handleLogout = async () => {
+    try {
+      await logout();
+    } catch (error) {
+      console.error("Error during logout:", error);
+    }
+  };
 
   // Mostrar las opciones según el rol
   const renderMenuItems = () => {
@@ -48,14 +62,16 @@ const ExtraHoursMenu = () => {
         return (
           <>
             <div className="menu-item" onClick={() => navigate("/add")}>
-              <img src={Agregar} alt="Agregar" />
+              <div id="imgagregar">
+                <img src={Agregar} alt="Agregar" />
+              </div>
               <p>Registrar Horas Extra</p>
             </div>
             <div
               className="menu-item"
               onClick={() => navigate("/ManagementExtraHour")}
             >
-              <img src={Gestion} alt="Gestión de horas" />
+              <img src={Gestion} alt="Gestion de horas" />
               <p>Gestionar Registro de Horas</p>
             </div>
             <div className="menu-item" onClick={() => navigate("/reports")}>
@@ -68,17 +84,25 @@ const ExtraHoursMenu = () => {
             </div>
           </>
         );
-
       default:
         return null;
     }
   };
 
   return (
-    <div id="extrahourmenu-page">
+    <div>
       <div className="menu">
         <h1>Sistema de Gestión de Horas Extra</h1>
         <div className="grid">{renderMenuItems()}</div>
+        {/* <div className="button-container">
+          <button className="logout-button" onClick={handleLogout}>
+            Cerrar Sesión
+          </button>
+          <button className="change-password-button" onClick={openModal}>
+            Cambiar Contraseña
+          </button>
+        </div> */}
+        {isModalOpen && <ChangePasswordModal onClose={closeModal} />}
       </div>
     </div>
   );
