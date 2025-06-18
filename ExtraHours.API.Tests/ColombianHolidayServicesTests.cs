3fd075ac57ab263c450d099183df26c4851caba3
Andrés Felipe Toro González <pipetoro19@hotmail.com>
2025-06-17 22:23:22 -0500

    ""

--- ExtraHours.API.Tests/ColombianHolidayServicesTests.cs
+++ ExtraHours.API.Tests/ColombianHolidayServicesTests.cs
@@ -10,7 +10,7 @@ namespace ExtraHours.API.Tests
         [InlineData("2025-01-01", true)]  // Año Nuevo (fijo)
         [InlineData("2025-05-01", true)]  // Día del Trabajo (fijo)
         [InlineData("2025-01-06", false)] // Fecha original de Reyes Magos (cae lunes por defecto, revisar lógica)
-        [InlineData("2025-01-13", false)]  // Lunes siguiente si aplica traslado
+        [InlineData("2025-01-13", false)]  // Lunes siguiente si aplica traslado (Revisar lógica)
         [InlineData("2025-04-13", true)]  // Domingo (cualquier domingo debería ser festivo)
         [InlineData("2025-04-17", true)]  // Jueves Santo (dep. de Pascua)
         [InlineData("2025-04-18", true)]  // Viernes Santo (dep. de Pascua)
